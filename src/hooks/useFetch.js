8434687543b27f2fbdfa2099c23155e53317ca4b
Lenny Balston <8708781+LennyBalston@users.noreply.github.com>
2022-03-15 16:31:24 +0000

    Set timeout on API fetch

--- src/hooks/useFetch.js
+++ src/hooks/useFetch.js
@@ -1,32 +1,37 @@
 import { useState, useEffect } from "react";
 
 const useFetch = (url) => {
+  const controller = new AbortController();
+  const signal = controller.signal;
+
+  setTimeout(() => controller.abort(), 3000);
+
   const [isLoading, setIsLoading] = useState(true);
   const [isError, setIsError] = useState(false);
   const [isSuccess, setIsSuccess] = useState(false);
   const [data, setData] = useState(null);
 
   useEffect(() => {
-    fetch(url)
-    .then(res => {
-      if (res.ok) {
-        return res.text();
-      } else {
-        throw new Error()
-      }      
-    })
-    .then(data => {
-      setData(JSON.parse(data));
-      setIsSuccess(true);
-      setIsLoading(false);
-    })
-    .catch(err => {
-      setIsError(true);
-      setIsLoading(false);
-    })
-  }, [url]);
+    fetch(url, { signal })
+      .then(res => {
+        if (res.ok) {
+          return res.text();
+        } else {
+          throw new Error()
+        }
+      })
+      .then(data => {
+        setData(JSON.parse(data));
+        setIsSuccess(true);
+        setIsLoading(false);
+      })
+      .catch(err => {
+        setIsError(true);
+        setIsLoading(false);
+      })
+  }, [url]); // eslint-disable-line react-hooks/exhaustive-deps
 
-  return {data, isLoading, isError, isSuccess};
+  return { data, isLoading, isError, isSuccess };
 };
 
 export default useFetch;
