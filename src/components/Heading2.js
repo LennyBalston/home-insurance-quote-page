20fab4a3f4774474bc98756b000aca09e19d194c
Lenny Balston <8708781+LennyBalston@users.noreply.github.com>
2022-03-15 15:15:28 +0000

    Reduce <h2> size to fit add-on title on single line for desktop

--- src/components/Heading2.js
+++ src/components/Heading2.js
@@ -3,7 +3,7 @@ import styled from 'styled-components';
 const Heading2 = styled.h2`
   color: ${(props) => props.theme.font.heading2.color};
   color: ${(props) => props.color && props.theme[props.color]};
-  font-size: ${(props) => props.theme.font.heading2.size};
+  /* font-size: ${(props) => props.theme.font.heading2.size}; */
   font-weight: ${(props) => props.theme.font.heading2.fontWeight};
   letter-spacing: ${(props) => props.theme.font.heading2.letterSpacing};
 
